4bb2b0c6ccdb5f0f026580e6cedc0b2b95b1dcc9
v1_陳萱芮 <xuanruichen5@gmail.com>
2024-12-09 16:46:36 +0800

    v2_陳萱芮

--- app/src/main/java/com/example/s1122039/MainActivity.kt
+++ app/src/main/java/com/example/s1122039/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.s1122039
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.s1122039.ui.theme.S1122039Theme
+import androidx.compose.foundation.layout.Arrangement
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +45,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    var horizontalAlignment = Alignment.CenterHorizontally
+
+    Column (modifier = Modifier.fillMaxSize().background(Color.Green),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+
+        Text(text = "2024期末上機考(資管二B陳萱芮)\n",)
+        Text(text = "遊戲持續時間 0 秒\n",)
+        Text(text = "您的成績 0 分\n",)
+
+        Box {
+                Modifier
+                    .background(Color(0xff95fe95))
+
+                Image(
+                    painter = painterResource(id = R.drawable.class_b),
+                    contentDescription = "B班",
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            Button(onClick = {})
+            {
+                Text(text = "結束App")
+            }
+            }
+        }
+    }
+
+
+
+
+
 
 @Preview(showBackground = true)
 @Composable
@@ -45,3 +86,6 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+
+
